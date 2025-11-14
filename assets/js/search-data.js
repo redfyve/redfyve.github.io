4424d7561deee59d19874c558d5da4551fb08c7c
redfyve <65499010+redfyve@users.noreply.github.com>
2025-11-14 06:36:41 +0000

    Deploying to gh-pages from @ redfyve/redfyve.github.io@a42e314cd55dbf4b83f434e4d28e1056cab3ecf1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-resume",
+          title: "resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "post-a-post-with-formatting-and-links",
         
